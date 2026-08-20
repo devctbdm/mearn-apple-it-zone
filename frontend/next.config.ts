53d54d3a6a8b1f7a8e259a8fac4a7c7747b9e96d
devctbdm <devctme@gmail.com>
2026-08-20 22:37:38 +0600

    fix build issue

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from 'next';
-import { defineConfig, globalIgnores } from 'eslint/config'
-import nextVitals from 'eslint-config-next/core-web-vitals'
 
 const nextConfig: NextConfig = {
-  ...nextVitals,
   /* config options here */
   images: {
     remotePatterns: [
@@ -21,19 +18,11 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  ...globalIgnores([
-    // Default ignores of eslint-config-next:
-    '.next/**',
-    'out/**',
-    'build/**',
-    'next-env.d.ts',
-  ]),
 
   typescript: {
     // Dangerously allow production builds to successfully complete even if your project has type errors.
     ignoreBuildErrors: true,
   },
- 
 };
 
 export default nextConfig;
